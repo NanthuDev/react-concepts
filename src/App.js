29d6c66fb3d1f92bd7aea238d673abac09b18a46
Nantha <nanthu1593@gmail.com>
2024-08-31 23:35:30 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import logo from './logo.svg';
+ import { useState } from 'react';
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
+  const [timer,setTimer] = useState(0)
+  let setInter = null;
+
+
+  useEffect(()=>{
+    
+  },[])
+
+  const start=()=>{
+    TimerControl();
+  }
+  const stop=()=>{ 
+  }
+
+  const TimerControl = ()=>{
+    setInter = setInterval(()=>{
+      setTimer(timer+1);
+    },1000);
+  }
   return (
     <div className="App">
-      <p></p>
-     <input type="button" value="start"/> 
-     <input type="button" value="Stop"/> 
+      <p>{timer}</p>
+     <input type="button" value="start" onClick={start}/> 
+     <input type="button" value="Stop" onClick={stop}/> 
 
     </div>
   );
