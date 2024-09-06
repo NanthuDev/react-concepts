d093f65910e56e95f2f58303510915b65b6c7975
Nantha <nanthu1593@gmail.com>
2024-09-06 22:36:54 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,45 +1,26 @@
- import { useState } from 'react';
-import './App.css';
-import { useEffect } from 'react';
-let setInter = null;
+ import './App.css';
+ import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Routes
+} from "react-router-dom";
+import Timer from './pages/timer';
 
+
+ 
 function App() {
-  const [timer,setTimer] = useState(0)
-  const [start,setStop] = useState(false)
  
 
 
-  useEffect(()=>{  
-
-    if(start === true){
-      console.log( "Started");
-      TimerControl(); 
-    }else{
-      console.log( "Stopped / Not yet start"); 
-      clearInterval(setInter); 
-    }
-    return ()=>clearInterval(setInter)
-  },[ start])
-
-  const startTimer=()=>{
-  //  TimerControl();
-    setStop(true);
-  }
-  const stop=()=>{ 
-   // clearInterval(setInter);
-    setStop(false)
-  }
-
-  const TimerControl = ()=>{
-    setInter = setInterval(()=>{
-      setTimer(timer=>timer+1);
-    },1000);
-  }
+ 
   return (
     <div className="App">
-      <p>{timer}</p>
-     <input type="button" value="start" onClick={startTimer}/> 
-     <input type="button" value="Stop" onClick={stop}/> 
+       
+       <Routes>
+        <Route path="/" /> {/* 👈 Renders at /app/ */}
+      </Routes>
 
     </div>
   );
